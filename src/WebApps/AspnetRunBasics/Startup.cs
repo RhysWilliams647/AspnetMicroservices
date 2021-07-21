754a8865179a035322c9b0cfd3d90b58f5d6b665
RhysWilliams647 <rhyswilliams647@googlemail.com>
2021-07-21 19:29:21 +0100

    Web status app

--- src/WebApps/AspnetRunBasics/Startup.cs
+++ src/WebApps/AspnetRunBasics/Startup.cs
@@ -1,9 +1,12 @@
 using AspnetRunBasics.Services;
 using Common.Logging;
+using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
 using Polly;
 using Polly.Extensions.Http;
@@ -44,6 +47,13 @@ namespace AspnetRunBasics
                 .AddPolicyHandler(GetCircuitBreakerPolicy());
 
             services.AddRazorPages();
+
+            services.AddHealthChecks()
+                .AddUrlGroup(
+                    new Uri($"{Configuration["ApiSettings:GatewayAddress"]}/swagger/index.html"),
+                    "Ocelot API Gw",
+                    HealthStatus.Degraded
+                );
         }
 
         private static IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
@@ -93,6 +103,11 @@ namespace AspnetRunBasics
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
+                endpoints.MapHealthChecks("/hc", new HealthCheckOptions()
+                {
+                    Predicate = _ => true,
+                    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+                });
             });
         }
     }
